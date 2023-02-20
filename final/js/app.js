c9785a5a89ef6ab62f6eb33a39b2d74da1ac760e
TOLULOPE OLUWABIYI <TOLULOPE.OLUWABIYI@TRANTERIT.LOCAL>
2023-02-20 12:52:42 +0100

    3rd commit

--- final/js/app.js
+++ final/js/app.js
@@ -10,3 +10,10 @@ const getElement = (selector) => {
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
 
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
